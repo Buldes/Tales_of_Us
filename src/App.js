a8b2098f1a5c8e476c576df73908561b1961e56a
Buldes <115708157+Buldes@users.noreply.github.com>
2024-09-11 10:55:42 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import data from "./data/data.json"
+
 
 function App() {
+  const [jsonContent] = useState(JSON.stringify(data, null, 2)); // Um die Daten formatiert anzuzeigen
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Tales of us">
       </header>
+        <h>Test # 2</h>
+        <label>{jsonContent ? jsonContent: 'Json content will appear here'}</label>
     </div>
   );
 }
